e032e2b6355d7341069c6459fd0b28763123990e
peepeds <edbertmac@gmail.com>
2023-01-03 21:52:04 +0700

    first

---
+++ volim.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std ; 
+
+#define max 8 
+#define min 1 
+
+int main(){
+    int a , b , set = 0 , i = 0   , d; 
+    char c ; 
+    cin >> a ; 
+    cin >> b ;
+
+    for(i=0 ; i < b ;i++){
+        cin >> d >> c ; 
+        set += d ;
+        if(c=='T'&& set <= 210){
+            a++ ; 
+            if(a< min ){
+                a = max ;
+            } else if ( a > max ){
+                a = min ;
+            }
+        }
+    }
+    cout  <<  a << endl ;
+
+}
