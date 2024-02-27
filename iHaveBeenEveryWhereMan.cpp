5578eaaaa2dd3719126e6b404422b5a7b071fe2a
peepeds <edbertmac@gmail.com>
2024-02-27 23:25:14 +0700

    feat: add iHaveBeenWveryWhereMan

--- iHaveBeenEveryWhereMan.cpp
+++ iHaveBeenEveryWhereMan.cpp
@@ -1,24 +1,51 @@
 #include <iostream>
-#include <string.h>
+#include <vector>
+#include <set>
+#include <algorithm>
 using namespace std ; 
 
-int main(){
-    int i , a  , b ,j , k , l ,  c = 0; 
-    cin >> a ; 
-    
-    for(i=0;i<a;i++){
-        cin >> b ; 
-        string s[b];
-        for(j=0 ; j < b ;j++){
-            cin >>s[b];
-        }
-        for(k=0 ; k < b -1 ; k++){
-            for(l=k+1;l<b;l++){
-                if(strcmp(s[k],s[l])){
-                    string  tmp ; 
-
-                }
+void usingVector(int x){
+    int  n; 
+    int size ; 
+    string city ; 
+   
+    cin >> x ; 
+    for(int i = 0 ; i < x ; i++){
+        vector<string> visit ;
+        cin >> n ; 
+        for(int j = 0 ; j < n ; j++){
+            cin >> city ;
+            auto it = find(visit.begin(), visit.end(),city);
+            if(it == visit.end()){
+                visit.push_back(city);
             }
         }
+        size = visit.size();
+        cout << size << endl ;
+
     }
+}
+
+void usingSet(int x){
+    
+    int  n;
+    string city;
+
+    
+    for (int i = 0; i < x; ++i) {
+        cin >> n;
+        set<string> visit;
+        for (int j = 0; j < n; ++j) {
+            cin >> city;
+            visit.insert(city); // insert city to set
+        }
+        cout << visit.size() << endl;
+    }
+
+}
+
+int main(){
+    int x ;
+    cin >> x;
+    usingSet(x);
 }
