3a0b902b6626c5441b9c4a83188a0d94bf75a5e6
peepeds <edbertmac@gmail.com>
2024-03-05 22:16:28 +0700

    feat: add aDifferentProblem.cpp

---
+++ aDifferentProblem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std ; 
+
+int main(){
+    long long int a , b , c ; 
+    
+    while(cin >> a >> b ){
+        c = a - b ; 
+        c = abs(c);
+        cout << c  << endl ;
+    }
+
+    
+}
